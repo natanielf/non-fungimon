0fdcb95b2296bd21c2c0eb0fb933755485e45bdf
1856362 <1856362@01-19955.pusd.dom>
2022-01-03 09:18:30 -0800

    Test

--- src/Driver.java
+++ src/Driver.java
@@ -3,8 +3,6 @@ public class Driver {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-		System.out.println("Greetings, earthlings!");
-		System.out.println("Salutations, homo sapiens!");
 	}
 
 }
