84864e50c081e863e5667f0a50ffd08e25604da4
natanielf <natanielfarzan@gmail.com>
2022-01-03 09:33:36 -0800

    Generate NFT constructor

--- src/NFT.java
+++ src/NFT.java
@@ -1,14 +1,18 @@
 
 public class NFT {
-	
+
 	private String name;
-	private int rarity;
-	
-	private int level;
-	private boolean evo;
-	
-	private boolean health;
-	private boolean isPoisoned;
-	private boolean isStunned;
-	
+	private int rarity, level, health;
+	private boolean evo, isPoisoned, isStunned;
+
+	public NFT(String name, int rarity, int level, int health, boolean evo, boolean isPoisoned, boolean isStunned) {
+		this.name = name;
+		this.rarity = rarity;
+		this.level = level;
+		this.evo = evo;
+		this.health = health;
+		this.isPoisoned = isPoisoned;
+		this.isStunned = isStunned;
+	}
+
 }
