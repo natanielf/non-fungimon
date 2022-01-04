debcfe1d02bbf091e555fedbb3bf54f90c08588d
natanielf <natanielfarzan@gmail.com>
2022-01-04 09:15:26 -0800

    Add arrow key logic

--- src/Frame.java
+++ src/Frame.java
@@ -15,6 +15,8 @@ public class Frame extends JPanel implements KeyListener, ActionListener {
 	Tile b;
 	Player p;
 
+	boolean ctrlKeyPressed;
+	JFrame f;
 	Timer t;
 
 	public void paint(Graphics g) {
@@ -22,31 +24,61 @@ public class Frame extends JPanel implements KeyListener, ActionListener {
 	}
 
 	public Frame() {
-		JFrame f = new JFrame("Non-Fungimon");
+		f = new JFrame("Non-Fungimon");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(1920, 1080);
+		f.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		f.add(this);
 		f.addKeyListener(this);
 		t = new Timer(16, this);
 		t.start();
 		f.setVisible(true);
+
+		ctrlKeyPressed = false;
+
 		b = new Tile();
 		p = new Player();
 	}
 
 	public static void main(String[] arg) {
-		Frame f = new Frame();
+		Frame frame = new Frame();
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-
+		// key codes: 38 = up, 39 = right, 40 = down, 37 = left
+		switch (e.getKeyCode()) {
+		case 38:
+			System.out.println("up");
+			p.move(0);
+			break;
+		case 39:
+			System.out.println("right");
+			p.move(1);
+			break;
+		case 40:
+			System.out.println("down");
+			p.move(2);
+			break;
+		case 37:
+			System.out.println("left");
+			p.move(3);
+			break;
+		case 17:
+			ctrlKeyPressed = true;
+			break;
+		default:
+			System.out.println("other: " + e.getKeyCode());
+			break;
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// Quit the game the Q key (key 81) is released
-		if (e.getKeyCode() == 81)
+		// Quit with CTRL+Q activation
+		if (e.getKeyCode() == 17)
+			ctrlKeyPressed = false;
+
+		if (e.getKeyCode() == 81 && ctrlKeyPressed)
 			System.exit(0);
 	}
 
