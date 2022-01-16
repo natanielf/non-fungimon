85c0934538687192fb3e32f973f84eb942dadaf8
K-P-23 <89217688+K-P-23@users.noreply.github.com>
2022-01-15 17:51:08 -0800

    changed Type to isSolid

--- src/Tile.java
+++ src/Tile.java
@@ -1,21 +1,23 @@
 public class Tile {
 
 	// 0 = ground, 1 = moss, 2 = grass, 3 = rock, 4 = water
-	private int type;
+	
+	private String image;
+	private boolean isSolid;
 	private double chance;
 
 	public Tile() {
-		this.type = 0;
+		this.isSolid = false;
 		this.chance = 0;
 	}
 
 	public Tile(int type, double chance) {
-		this.type = type;
+		this.isSolid = isSolid;
 		this.chance = chance;
 	}
 
-	public int getType() {
-		return type;
+	public boolean getIsSolid() {
+		return isSolid;
 	}
 
 	public void setType(int type) {
